5c971715e934819797301c205c6e8673461cc28f
nickyoung <nickyoung@192.168.0.2>
2018-04-25 12:48:40 +1000

    Stock monitor now creates a view and updates itself

--- src/StockMonitor.java
+++ src/StockMonitor.java
@@ -1,18 +1,39 @@
 import javax.swing.*;
 
-public class StockMonitor extends JFrame implements Observer {
+public class StockMonitor implements Observer {
 
 	private Stock stock;
 	private JLabel symbolLabel, lastPriceLabel, dateLabel, timeLabel;
+	private JFrame frame;
 	
 	public StockMonitor(Stock stock) {
 		this.stock = stock;
 		this.stock.registerObserver(this);
+		
+		frame = new JFrame(stock.getSymbol() + " Monitor");
+		frame.setSize(600, 200);
+		JPanel panel = new JPanel();
+		symbolLabel = new JLabel("Symbol: " + stock.getSymbol());
+		lastPriceLabel = new JLabel("Last Price:");
+		dateLabel = new JLabel("Date: ");
+		timeLabel = new JLabel("Time: ");
+		
+		panel.add(symbolLabel);
+		panel.add(lastPriceLabel);
+		panel.add(dateLabel);
+		panel.add(timeLabel);
+		
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	@Override
 	public void update() {
 		System.out.println("\n" + stock.getSymbol() + " " + stock.getLastTrade() + " " + stock.getDate() + " " + stock.getTime());
+		lastPriceLabel.setText("Last Price: " + stock.getLastTrade());
+		dateLabel.setText("Date: " + stock.getDate());
+		timeLabel.setText("Time: " + stock.getTime());
 	}
 
 }
