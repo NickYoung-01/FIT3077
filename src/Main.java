19b801f69d5752e51c661e39c9cc6ce3d3f2ee46
nickyoung <nickyoung@192.168.0.2>
2018-04-25 12:50:01 +1000

    Added a general TODO list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 
 public class Main {
 	
+	/*
+	 * TODO:
+	 * 1. If they want to view the same stock twice, don't keep creating a new stock object
+	 * 		query the arraylist of stocks and find the one that matches what we want.
+	 * 2. Adjust the timer to refresh evrey 5mins
+	 */
+	
 	public static void main(String[] args) {
 		MainView view = new MainView();
 		Controller controller = new Controller(view);
