705f762dffbc5485a68fae2fcc1c5c9e4ac1dbf0
nickyoung <nickyoung@192.168.0.2>
2018-04-25 12:46:16 +1000

    This is where the program starts

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		MainView view = new MainView();
+		Controller controller = new Controller(view);
+		
+		view.setVisible(true);
+		
+		
+	}
+
+}
