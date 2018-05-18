298dd166bd48be0d597d12e5d0cffff234cf9946
nickyoung <nickyoung@192.168.0.6>
2018-05-19 09:51:12 +1000

    refactored

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -15,7 +15,7 @@ public class Controller {
 	private HomeView view;
 	private int existingIndex;
 	private List<Stock> stockList = new ArrayList<Stock>();
-	private ServerLive serverWSDL = new ServerLive();
+	private ServerLive serverLive = new ServerLive();
 	private ServerHistoric serverHistoric = new ServerHistoric();
 	
 	public Controller(HomeView view) {
@@ -44,6 +44,7 @@ public class Controller {
 		return false;
 	}
 	
+	//time limit is given in seconds
 	public void createStock(String symbol, ServerAbstract server, int timeLimit){
 		stockList.add(new Stock(symbol, server, timeLimit));
 	}
@@ -52,16 +53,18 @@ public class Controller {
 		//create text monitor
 		if (monitorIndexType == 0) {
 			new StockMonitor(stock);
-		} else {
+		} else if (monitorIndexType == 1) {
 			//create graph monitor
 			new GraphMonitor(stock);
 		}
 	}
 	
+	//Listener for the combo box for the service list
 	class ServiceTypeListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			//User has clicked an option in the combo box
 			view.handleServiceChange(view.getServiceTypeIndex());
 		}
 		
@@ -77,36 +80,40 @@ public class Controller {
 			String inputText = "";
 			
 			try {
+				//If we want a live stock monitor
 				if (view.getServiceTypeIndex() == 0) {
 					inputText = view.getInputText().toUpperCase();
+					//make sure that the input text is 3 or less
+					//standard size of stock ticker
 					if(inputText.length() <= 3) {
 					//the stock exists, so just add another observer to it
 						if (stockIsBeingMonitored(inputText)) {
 							createMonitor(stockList.get(existingIndex), monitorIndexType);
 						} else {
 							//stock doesn't exists, create a new stock and monitor
-							createStock(inputText, serverWSDL, 60 * 5);
+							createStock(inputText, serverLive, 60 * 5);
 							//Remove stock if invalid
-							int ind = stockList.size()-1;
-							if (!stockList.get(ind).isValid()){
-								stockList.remove(ind);
+							int lastIndex = stockList.size()-1;
+							if (!stockList.get(lastIndex).isValid()){
+								stockList.remove(lastIndex);
 								view.displayErrorMessage("Invalid Symbol");
 							} else{
-								createMonitor(stockList.get(ind), monitorIndexType);
+								createMonitor(stockList.get(lastIndex), monitorIndexType);
 							}	
 						}
 					} else {
 						view.displayErrorMessage("Input limit is 3 chars");
 					}
 				} else {
-					//this is timelapse stuff
+					//this is historic server
+					//if stock is being monitored just create a new observer
 					if (stockIsBeingMonitored(view.getSelectedHistoricStock())) {
 						createMonitor(stockList.get(existingIndex), monitorIndexType);
 					} else {
-						System.out.println(view.getSelectedHistoricStock());
+						//create a new stock for it and then the observer
 						createStock(view.getSelectedHistoricStock(), serverHistoric, 5);
-						int ind = stockList.size()-1;
-						createMonitor(stockList.get(ind), monitorIndexType);
+						int lastIndex = stockList.size()-1;
+						createMonitor(stockList.get(lastIndex), monitorIndexType);
 					}
 				}
 			} catch (Exception ex) {
